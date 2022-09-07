58befdc74aa30ae865141b316f2e85816f5da35e
HectorLara <hectorlaras.18@gmail.com>
2022-09-07 11:09:50 -0500

    se actualiza programa de convertir monedas y se agrega funcion de convertir a dolar

--- Convertirmoneda.py
+++ Convertirmoneda.py
@@ -1,7 +1,37 @@
-pesos = input("Ingrese el valor en pesos:" )
-pesosFloat = float(pesos)
-valor_dolar = 20
-dolares = pesosFloat/valor_dolar
-dolares = round(dolares,1)
-strDolares = str(dolares)
+from asyncio.windows_events import NULL
+
+
+menu = """
+Bienvenidos al convrsor de monedas 🪙
+
+1 - Peso colombiano
+2 - Peso argentino
+3 - Peso mexicano
+
+Elige una opcion: 
+"""
+strDolares = NULL
+
+##funciones
+def ConvertirMoneda(valorRef):
+    valorLocal = NULL
+    pesos = input("Ingrese el valor en pesos:" )
+    pesosFloat = float(pesos)
+    valor_dolar = valorRef
+    dolares = pesosFloat/valor_dolar
+    dolares = round(dolares,1)
+    valorLocal = str(dolares)
+    return valorLocal
+
+opcion = input(menu)
+
+if opcion == "1":
+    strDolares = ConvertirMoneda(60)
+elif opcion == "2":
+    strDolares = ConvertirMoneda(100)
+else:
+    strDolares = ConvertirMoneda(20)
+
+
+
 print("Tienes $" + strDolares + " dolares")
---
+++ condicionales.py
@@ -0,0 +1,10 @@
+##condicional if 
+
+##los dos puntos despues de condicionales viene los dos puntos que significa que estas dentro del bucle
+##adicional se tienen que dejar 4 espacios para que python sepa que el codigo pertence al condicional se le conoce como identacion
+
+edad = int(input("Ingresa tu edad: "))
+if edad > 17:
+    print("bienvenido")
+else:
+    print("Espera a tener 18 o mas")
